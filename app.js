2cece64cc50b57c9d02a3199dc713f304751941f
Your Name <you@example.com>
2023-06-10 23:57:39 -0700

    adding autoIndex db connection option and recator of app.js

--- app.js
+++ app.js
@@ -6,26 +6,27 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const router = require('./router')
 
-mongoose.connect(process.env.MONGO_DB_URL);
-const database = mongoose.connection;
+async function init() {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      autoIndex: true
+    });
 
-database.on('error', (error) => {
-  console.log(process.env.MONGO_DB_URL)
-  console.log(error);
-});
+    const app = express();
 
-const app = express();
+    app.use(bodyParser.json());
+    app.use(express.static(__dirname + '/public'));
+    app.use(router);
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}`);
+    });
 
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
-app.use(router);
+  } catch (err) {
+    console.log(process.env.MONGO_DB_URL)
+    console.log(err);
+  }
+}
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.set('view engine', 'ejs');
-
-database.once('connected', () => {
-  console.log('Database Connected');
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-  });
-});
+init();
